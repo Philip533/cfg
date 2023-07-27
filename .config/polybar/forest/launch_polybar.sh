ad7b7554d116eca32a732421792b231ab7976b0f
Philip Jones <philip.jones533@gmail.com>
2023-07-27 11:32:44 +0100

    Minimal polybar setup

--- .config/polybar/forest/launch_polybar.sh
+++ .config/polybar/forest/launch_polybar.sh
@@ -2,8 +2,8 @@
 killall -q polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -c ~/.config/polybar/forest/config.ini -q second &
+    MONITOR=$m polybar -c ~/.config/polybar/forest/config.ini -q main &
   done
 else
-  polybar -c ~/.config/polybar/forest/config.ini -q second &
+  polybar -c ~/.config/polybar/forest/config.ini -q main &
 fi
